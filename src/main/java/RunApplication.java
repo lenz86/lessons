4e66414e3dfdb06b31689a983de03875b2650647
LenzWork <kursalex92@gmail.com>
2022-03-29 13:09:37 +0300

    before add new branch: logger

--- src/main/java/RunApplication.java
+++ src/main/java/RunApplication.java
@@ -2,5 +2,6 @@ public class RunApplication {
 
     public static void main(String[] args) {
         System.out.println("Master branch in new project");
+        System.out.println("Create a logger lessons branch");
     }
 }
